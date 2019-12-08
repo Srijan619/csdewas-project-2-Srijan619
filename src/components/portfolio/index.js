70f599a4ecef67a6b251cc543a6dc68cdbe8b8ed
Srijan-Desktop <chapssrijan@gmail.com>
2019-12-08 22:13:44 +0200

    Test changes

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -15,12 +15,14 @@ class index extends Component {
             unitValue: '',
             quantity: '',
             totalValue: '',
-            isChecked: false,
-            stockArray: []
+            portfolioValue: '',
+            checkbox: false,
+            stockArray: [],
+            checkedItems: []
 
         };
 
-        
+
         this.handleChecked = this.handleChecked.bind(this);
         this.handleFormReset = this.handleFormReset.bind(this);
         this.hanleAdd = this.handleAdd.bind(this);
@@ -37,7 +39,18 @@ class index extends Component {
         }))
     }
 
+    calculateTotalValue(total_value) {
+        const cparray = Object.assign([], this.state.stockArray);
 
+        for (var i = 0; i < cparray.length; i++) {
+
+            total_value = total_value + parseFloat(cparray[i].totalValue);
+        }
+        console.log(total_value)
+        this.setState({
+            portfolioValue: total_value
+        })
+    }
     handleFormChange = (evt) => {
         this.setState({ [evt.target.name]: evt.target.value });
     }
@@ -45,45 +58,77 @@ class index extends Component {
         event.preventDefault();
         this.stockID = this.stockID + 1;
         const cparray = Object.assign([], this.state.stockArray);
-
+        const total = this.state.quantity * this.state.unitValue;
+        this.calculateTotalValue(total);
         cparray.push({
             ids: this.stockID,
-            isChecked: this.state.select,
+            checkbox: this.state.checkbox,
             stockName: this.state.stockName,
             unitValue: this.state.unitValue,
             quantity: this.state.quantity,
+            totalValue: total,
+            portfolioValue: this.state.portfolioValue
         })
+
         this.setState({
             stockArray: cparray
         })
+
+
     }
 
-    handleChecked () {
-        this.setState({
-            id:this.state.id,
-            isChecked: !this.state.isChecked,
-           });
-       
-      }
-    handleRemove = (index) => {
-        event.preventDefault();
+    handleChecked(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        
         const cparray = Object.assign([], this.state.stockArray);
         for(var i=0;i<cparray.length;i++)
         {
-          if(cparray[i].isChecked===true)
+        cparray[i].checkbox=value
+        this.setState({
+           
+        })
+    }
+    }
+    handleRemove = (index) => {
+        event.preventDefault();
+        /*
+        const cparray = Object.assign([], this.state.stockArray);
+        const selectedData=[];
+        for(var i=0;i<cparray.length;i++)
+        {
+          if(cparray[i].checkbox===true)
           {
-            cparray.splice(index, 1);
-            this.setState({
-                stockArray: cparray
-            })
+            console.log("Selected"+cparray[i].stockName);
+            selectedData.push(cparray[i])
+          
           }  
         }
-       
+        this.setState({
+            selectedData:[],
+            stockArray: selectedData
+        })
+
+        */
+        for (var i = 0; i < this.state.checkedItems.length; i++) {
+            // Convert id from string back to integer.
+            let index = parseInt(this.state.checkedItems[i])
+            // Replace the item to delete with empty string, so that every
+            // other element remains in place.
+            this.state.checkbox[index] = '';
+        }
+        // Re-render the component by calling setState.
+        this.setState({
+            checkedItems: [],
+            checkbox: this.state.checkbox
+        });
 
     }
 
+
     render() {
-        const { stockArray, stockName, quantity, unitValue } = this.state;
+        const { stockArray, stockName, quantity, unitValue, portfolioValue } = this.state;
         const isEnabled = (stockName.length && quantity.length && unitValue.length) > 0;
         return (
             <div className="container">
@@ -111,24 +156,26 @@ class index extends Component {
                                 </tr>
                             </thead>
                             {stockArray.map((post, index) => {
-                                 return(
-                            <tbody>
-                                <tr key={post.id}>
-                                    <td id="name">{post.stockName}</td>
-                                    <td>{post.unitValue}</td>
-                                    <td>{post.quantity}</td>
-                                    <td>{this.props.totalValue}</td>
-                                    <td><input type="checkbox" name="checkbox" onChange={this.handleChecked} checked={this.state.isChecked} ></input></td>
-                                </tr>
-                            </tbody>
-                                 )
+                                return (
+
+
+                                    <tbody key={post.ids}>
+                                        <tr>
+                                            <td style={{textTransform:"uppercase"}}>{post.stockName} </td>
+                                            <td>{post.unitValue}</td>
+                                            <td>{post.quantity}</td>
+                                            <td>{post.totalValue}</td>
+                                            <td><input type="checkbox" name="checkbox" onChange={this.handleChecked}  ></input></td>
+                                        </tr>
+                                    </tbody>
+                                )
                             })}
-                              
+
                         </table>
                     </div>
                 </div>
                 <form ref="stock">
-                    <div className="tableWrapper">
+                    <div className="tableWrapper" style={{ overflow: "hidden" }}>
 
                         <input type="text" name="stockName" onChange={this.handleFormChange} placeholder="Name" id="name"></input>
                         <input type="number" name="unitValue" onChange={this.handleFormChange} placeholder="Unit Value"></input>
@@ -136,7 +183,7 @@ class index extends Component {
 
                     </div>
                     <div className="Header">
-                        <span>Total value of Portfolio 1:<span id="amount"></span> 968.20 $</span>
+                        <span>Total value of Portfolio 1:<span id="amount"></span> {portfolioValue}</span>
                     </div>
                     <div className="Header">
                         <button onClick={this.handleAdd} disabled={!isEnabled} className="button buttonAdd" type="submit">Add Stock</button>
