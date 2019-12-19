2a23625555bd31a4506c10d108551e2738f1479a
Srijan <chapssrijan@gmail.com>
2019-12-19 13:04:02 +0200

    Updates

---
+++ precache-manifest.0c1cacc16b079dd5595c5e3a3ffbc90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2addb50d5e5130f768ba",
+    "url": "/csdewas-project-2-Srijan619/static/css/main.8e67beed.chunk.css"
+  },
+  {
+    "revision": "2addb50d5e5130f768ba",
+    "url": "/csdewas-project-2-Srijan619/static/js/main.2addb50d.chunk.js"
+  },
+  {
+    "revision": "e8eef6eddfb2188bf005",
+    "url": "/csdewas-project-2-Srijan619/static/js/1.e8eef6ed.chunk.js"
+  },
+  {
+    "revision": "feedc2efc2ac314c157c",
+    "url": "/csdewas-project-2-Srijan619/static/js/runtime~main.feedc2ef.js"
+  },
+  {
+    "revision": "b65122070db4e1b77aecdadd11095da4",
+    "url": "/csdewas-project-2-Srijan619/static/media/cancel.b6512207.svg"
+  },
+  {
+    "revision": "1170ee91350950edf6c78ffebb7f632e",
+    "url": "/csdewas-project-2-Srijan619/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/csdewas-project-2-Srijan619/precache-manifest.0c1cacc16b079dd5595c5e3a3ffbc90e.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/csdewas-project-2-Srijan619/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
